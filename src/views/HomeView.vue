3efbb9b9d0b99106a1d95bc6aa21493ac3fa665a
Andreas <89839661+Andreasd21@users.noreply.github.com>
2022-09-21 09:51:18 +0200

    Initialize vue framework
    Adde the start of our vue frame work

---
+++ src/views/HomeView.vue
@@ -0,0 +1,27 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios';
+</script>
+
+  <script>
+export default {
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted(){
+    axios
+          .get('https://localhost:49153/WeatherForecast')
+          .then(response => (this.info = response))
+  }
+  
+};
+</script>
+<template>
+  <main>
+  testtt
+  {{info}}
+    <TheWelcome />
+  </main>
+</template>

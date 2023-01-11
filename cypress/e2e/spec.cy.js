5cbfe66eb999775ed56179a900bf27a1e195b5e5
Andreas <89839661+Andreasd21@users.noreply.github.com>
2023-01-11 11:24:45 +0100

    Images port fixed

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,13 @@
 describe('empty spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:80')
-    cy.wait(10000)
+    cy.wait(1000)
   })
 })
 
 describe('empty', () => {
   it('passes', () => {
     cy.visit('http://localhost:8081')
-    cy.wait(10000)
+    cy.wait(1000)
   })
 })
